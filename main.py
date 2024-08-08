93e7ce989663512d8ff29256f0e40b852900ca60
Anand Vardhan <anandvar669@gmail.com>
2024-08-08 06:20:35 +0530

    pdf Expert

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import streamlit as st
 import Experts.ats as ats
 import Experts.youtubeTranscripter as youtubeTranscripter
 import Experts.nutritionExpert as nutritionExpert 
+import Experts.anyPdfExpert as anyPdfExpert 
+
+
 
 st.set_page_config(page_title="Gemini Experts")
 
 st.markdown("<h1 style='text-align: center; padding: 50px; margin-top:0px;'>Gemini Experts</h1>", unsafe_allow_html=True)
 
-col1, col2, col3, = st.columns([1,1,1])  # Adjust column ratios as needed
+col1, col2, col3, col4 = st.columns([1.5,2,2,2])  # Adjust column ratios as needed
 
 with col1:
     if st.button("ATS Expert"):
@@ -19,6 +22,9 @@ with col2:
 with col3:
     if st.button("Nutrition Expert"):
         st.session_state.page = "nutritionExpert_page"
+with col4:
+    if st.button("Any Pdf Expert"):
+        st.session_state.page = "pdf_expert_page"
 
 
 
@@ -35,3 +41,6 @@ elif st.session_state.page == "youtube_transcripter_page":
 
 elif st.session_state.page == "nutritionExpert_page":
     nutritionExpert.nutritionExpert_page()
+
+elif st.session_state.page == "pdf_expert_page":
+    anyPdfExpert.pdf_expert_page()
